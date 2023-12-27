a80b223f95056964f8714297aec82af3ccbe9ae8
Gregory Higley <greg@prosumma.com>
2023-12-27 14:02:58 -0500

    Configuration

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import Lib
+import Database.PostgreSQL.Simple (connect, connectDatabase)
+import Prelude (putStrLn)
+import Prosumma.PG
+import Prosumma.Util.Environment
 import RIO
 
+-- PROMIGRATE_CONNECTION_STRING
+-- PROMIGRATE_DATABASE
+-- PROMIGRATE_METADATA_SCHEMA
+-- PROMIGRATE_METADATA_TABLE
+
+put :: a -> [a] -> [a]
+put x xs = xs ++ [x]
+
 main :: IO ()
-main = someFunc
+main = do 
+  database <- envString Nothing "PROMIGRATE_DATABASE"
+  let connectionString = "host=xyz database=postgres"
+  for_ (parseConnectInfo connectionString) $ \connectInfo -> do
+    let ci = connectInfo{connectDatabase=database}
+    conn <- connect connectInfo
+    liftIO $ putStrLn "ok"
